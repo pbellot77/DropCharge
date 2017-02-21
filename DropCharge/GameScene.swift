3abcf2b55fcb0b30438471925423c6bcd551cf2e
Patrick <patrickbellot77@gmail.com>
2017-02-21 17:05:44 -0600

    utilities

--- DropCharge/GameScene.swift
+++ DropCharge/GameScene.swift
@@ -9,81 +9,195 @@
 import SpriteKit
 import GameplayKit
 
+// Mark: - Game States
+enum GameStatus: Int {
+	case waitingForTap = 0
+	case waitingForBomb = 1
+	case playing = 2
+	case gameOver = 3
+}
+
+enum PlayerStatus: Int {
+	case idle = 0
+	case jump = 1
+	case fall = 2
+	case lava = 3
+	case dead = 4
+}
+
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
+	// MARK: - Properties
+	
+	var bgNode: SKNode!
+	var fgNode: SKNode!
+	var backgroundOverlayTemplate: SKNode!
+	var backgroundOverlayHeight: CGFloat!
+	var player: SKSpriteNode!
+	var platform5Across: SKSpriteNode!
+	var coinArrow: SKSpriteNode!
+	var lastOverlayPosition = CGPoint.zero
+	var lastOverlayHeight: CGFloat = 0.0
+	var levelPositionY: CGFloat = 0.0
+	var gameState = GameStatus.waitingForTap
+	var playerState = PlayerStatus.idle
+	
+	override func didMove(to view: SKView) {
+		let scale = SKAction.scale(to: 1.0, duration: 0.5)
+		fgNode.childNode(withName: "Ready")!.run(scale)
+		
+		setupNodes()
+		setupLevel()
+	}
+	
+	func setupNodes() {
+		let worldNode = childNode(withName: "World")!
+		
+		bgNode = worldNode.childNode(withName: "Background")!
+		backgroundOverlayTemplate = bgNode.childNode(withName: "Overlay")!.copy() as! SKNode
+		backgroundOverlayHeight = backgroundOverlayTemplate.calculateAccumulatedFrame().height
+		fgNode = worldNode.childNode(withName: "Foreground")!
+		player = fgNode.childNode(withName: "Player") as! SKSpriteNode
+		fgNode.childNode(withName: "Bomb")?.run(SKAction.hide())
+		platform5Across = loadForegroundOverlayTemplate("Platform5Across")
+		coinArrow = loadForegroundOverlayTemplate("CoinArrow")
+	}
+	
+	func setupLevel() {
+		
+	// Place initial platform
+		let initialPlatform = platform5Across.copy() as! SKSpriteNode
+		var overlayPosition = player.position
+		
+		overlayPosition.y = player.position.y - ((player.size.height * 0.5) +
+			(initialPlatform.size.height * 0.20))
+		initialPlatform.position = overlayPosition
+		fgNode.addChild(initialPlatform)
+		lastOverlayPosition = overlayPosition
+		lastOverlayHeight = initialPlatform.size.height / 2.0
+		
+	// Create random level
+		levelPositionY = bgNode.childNode(withName: "Overlay")!
+			.position.y + backgroundOverlayHeight
+		while lastOverlayPosition.y < levelPositionY {
+			addRandomForegroundOverlay()
+		}
+	}
+	
+	// MARK: - Overlay nodes
+	
+	func loadForegroundOverlayTemplate(_ fileName: String) -> SKSpriteNode {
+		let overlayScene = SKScene(fileNamed: fileName)!
+		let overlayTemplate = overlayScene.childNode(withName: "Overlay")
+		
+		return overlayTemplate as! SKSpriteNode
+	}
+	
+	func createForegroundOverlay(_ overlayTemplate: SKSpriteNode, flipX: Bool) {
+		let foregroundOverlay = overlayTemplate.copy() as! SKSpriteNode
+		
+		lastOverlayPosition.y = lastOverlayPosition.y + (lastOverlayHeight + (foregroundOverlay.size.height / 2.0))
+		lastOverlayHeight = foregroundOverlay.size.height / 2.0
+		foregroundOverlay.position = lastOverlayPosition
+			if flipX == true {
+				foregroundOverlay.xScale = -1.0
+			}
+		fgNode.addChild(foregroundOverlay)
+	}
+	
+	func createBackgroundOverlay() {
+		let backgroundOverlay = backgroundOverlayTemplate.copy() as! SKNode
+		
+		backgroundOverlay.position = CGPoint(x: 0.0, y: levelPositionY)
+		bgNode.addChild(backgroundOverlay)
+		levelPositionY += backgroundOverlayHeight
+	}
+	
+	func addRandomForegroundOverlay() {
+		let overlaySprite: SKSpriteNode!
+		let platformPercentage = 60
+		if Int.random(min: 1, max: 100) <= platformPercentage {
+			overlaySprite = platform5Across
+		} else {
+			overlaySprite = coinArrow
+		}
+		createForegroundOverlay(overlaySprite, flipX: false)
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		if gameState == .waitingForTap{
+			bombDrop()
+		}
+	}
+	
+	func bombDrop() {
+		gameState = .waitingForBomb
+		
+		// Scale out title & ready label
+		let scale = SKAction.scale(to: 0, duration: 0.4)
+		fgNode.childNode(withName: "Title")!.run(scale)
+		fgNode.childNode(withName: "Ready")!.run(
+			SKAction.sequence(
+				[SKAction.wait(forDuration: 0.2), scale]))
+		
+		// Bounce bomb
+		let scaleUp = SKAction.scale(to: 1.25, duration: 0.25)
+		let scaleDown = SKAction.scale(to: 1.0, duration: 0.25)
+		let sequece = SKAction.sequence([scaleUp, scaleDown])
+		let repeatSeq = SKAction.repeatForever(sequece)
+		fgNode.childNode(withName: "Bomb")!.run(SKAction.unhide())
+		fgNode.childNode(withName: "Bomb")!.run(repeatSeq)
+		run(SKAction.sequence([
+			SKAction.wait(forDuration: 2.0),
+			SKAction.run(startGame)]))
+	}
+	
+	func startGame() {
+		fgNode.childNode(withName: "Bomb")!.removeFromParent()
+		gameState = .playing
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // end of class
