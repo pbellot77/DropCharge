d4abd77849a1cc013b5717bde0d260f69945b5ae
Patrick <patrickbellot77@gmail.com>
2017-02-22 15:14:08 -0600

    fixed compile error and continued with project

--- DropCharge/GameScene.swift
+++ DropCharge/GameScene.swift
@@ -8,6 +8,17 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
+
+struct PhysicsCategory {
+	static let None: UInt32 = 0
+	static let Player: UInt32 = 0b1
+	static let PlatformNormal: UInt32 = 0b10
+	static let PlatformBreakable: UInt32 = 0b100
+	static let CoinNormal: UInt32 = 0b1000
+	static let CoinSpecial: UInt32 = 0b10000
+	static let Edges: UInt32 = 0b100000
+}
 
 // Mark: - Game States
 enum GameStatus: Int {
@@ -25,7 +36,7 @@ enum PlayerStatus: Int {
 	case dead = 4
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
 	// MARK: - Properties
 	
@@ -41,13 +52,42 @@ class GameScene: SKScene {
 	var levelPositionY: CGFloat = 0.0
 	var gameState = GameStatus.waitingForTap
 	var playerState = PlayerStatus.idle
+	let motionManager = CMMotionManager()
+	var xAcceleration = CGFloat(0)
+	
+	override func update(_ currentTime: TimeInterval) {
+		updatePlayer()
+	}
 	
 	override func didMove(to view: SKView) {
-		let scale = SKAction.scale(to: 1.0, duration: 0.5)
-		fgNode.childNode(withName: "Ready")!.run(scale)
-		
 		setupNodes()
 		setupLevel()
+		setupPlayer()
+		setupCoreMotion()
+		
+		physicsWorld.contactDelegate = self
+		
+		let scale = SKAction.scale(to: 1.0, duration: 0.5)
+		fgNode.childNode(withName: "Ready")!.run(scale)
+	}
+	
+	func didBegin(_ contact: SKPhysicsContact) {
+		let other = contact.bodyA.categoryBitMask == PhysicsCategory.Player ? contact.bodyB : contact.bodyA
+		switch other.categoryBitMask {
+		case PhysicsCategory.CoinNormal:
+			if let coin = other.node as? SKSpriteNode {
+				coin.removeFromParent()
+				jumpPlayer()
+			}
+		case PhysicsCategory.PlatformNormal:
+			if let _ = other.node as? SKSpriteNode {
+				if player.physicsBody!.velocity.dy < 0 {
+					jumpPlayer()
+				}
+			}
+		default:
+			break
+		}
 	}
 	
 	func setupNodes() {
@@ -82,6 +122,27 @@ class GameScene: SKScene {
 		while lastOverlayPosition.y < levelPositionY {
 			addRandomForegroundOverlay()
 		}
+	}
+	
+	func setupPlayer() {
+		player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width * 0.3)
+		player.physicsBody!.isDynamic = false
+		player.physicsBody!.allowsRotation = false
+		player.physicsBody!.categoryBitMask = PhysicsCategory.Player
+		player.physicsBody!.collisionBitMask = 0
+	}
+	
+	func setupCoreMotion() {
+		motionManager.accelerometerUpdateInterval = 0.2
+		let queue = OperationQueue()
+		motionManager.startAccelerometerUpdates(to: queue, withHandler: { accelerometerData, error in
+			guard let accelerometerData = accelerometerData else {
+				return
+			}
+			let acceleration = accelerometerData.acceleration
+			self.xAcceleration = (CGFloat(acceleration.x) * 0.75) +
+				(self.xAcceleration * 0.25)
+		})
 	}
 	
 	// MARK: - Overlay nodes
@@ -155,9 +216,66 @@ class GameScene: SKScene {
 	func startGame() {
 		fgNode.childNode(withName: "Bomb")!.removeFromParent()
 		gameState = .playing
+		player.physicsBody!.isDynamic = true
+		superBoostPlayer()
 	}
 	
+	func setPlayerVelocity(_ amount:CGFloat) {
+		let gain: CGFloat = 1.5
+		player.physicsBody!.velocity.dy = max(player.physicsBody!.velocity.dy, amount * gain)
+	}
 	
+	func jumpPlayer() {
+		setPlayerVelocity(650)
+	}
+	
+	func boostPlayer() {
+		setPlayerVelocity(1200)
+	}
+	
+	func superBoostPlayer() {
+		setPlayerVelocity(1700)
+	}
+	
+	func sceneCropAmount() -> CGFloat {
+		guard let view = self.view else {
+			return 0
+		}
+		let scale = view.bounds.size.height / self.size.height
+		let scaledWidth = self.size.width * scale
+		let scaledOverlap = scaledWidth - view.bounds.size.width
+		return scaledOverlap / scale
+	}
+	
+	func updatePlayer() {
+		
+		// Set velocity based on core motion
+		player.physicsBody?.velocity.dx = xAcceleration * 1000.0
+		
+		// Wrap player around edges of screen
+		var playerPosition = convert(player.position, from: fgNode)
+		let leftLimit = sceneCropAmount()/2 - player.size.width/2
+		let rightLimit = size.width - sceneCropAmount()/2 + player.size.width/2
+		if playerPosition.x < leftLimit {
+			playerPosition = convert(CGPoint(x: rightLimit, y: 0.0),
+			                         to: fgNode)
+			player.position.x = playerPosition.x
+	}
+		else if playerPosition.x > rightLimit {
+			playerPosition = convert(CGPoint(x: leftLimit, y: 0.0), to: fgNode)
+			player.position.x = playerPosition.x
+  }
+  // Check player state
+		if player.physicsBody!.velocity.dy < CGFloat(0.0) &&
+			playerState != .fall {
+				playerState = .fall
+				print("Falling.")
+		} else if player.physicsBody!.velocity.dy > CGFloat(0.0) &&
+				playerState != .jump {
+					playerState = .jump
+					print("Jumping.")
+		}
+	}
 	
 	
 	
